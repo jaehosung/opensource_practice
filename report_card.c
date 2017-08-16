9592df617669988144df04fd6924c8e7774e4e92
jaehosung <jaehossung@gmail.com>
2017-08-16 14:55:10 +0900

    report card : Print a message of introduction

--- report_card.c
+++ report_card.c
@@ -1,14 +1,7 @@
 #include<stdio.h>
-int main(){
-    int kor = 90, math = 80, eng = 100,sum,avg;
-    sum = kor + math + eng;
-    avg = sum/3;
 
-    printf("국어 : %d\n",kor);
-    printf("수학 : %d\n",math);
-    printf("영어 : %d\n",eng);
-    printf("합계 : %d\n",sum);
-    printf("평균 : %d\n",avg);
-
-    return 0;
+int main()
+{
+	printf("This program print report card.\n");
+	return 0;
 }
